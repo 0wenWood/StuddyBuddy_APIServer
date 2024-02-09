fe0107e9974c329936a14d6abf2c8da4ccb47d10
0wenWood <owenwood3435@outlook.com>
2024-02-09 10:15:52 -0500

    Fixing Bug 4

--- src/app.js
+++ src/app.js
@@ -18,8 +18,7 @@ app.use(groupsRouter);
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-All-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-All-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
