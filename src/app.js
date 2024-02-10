054233ebad34e5db28c00d117f1e4c1af1c56397
0wenWood <owenwood3435@outlook.com>
2024-02-10 15:19:51 -0500

    CORS Issue 4

--- src/app.js
+++ src/app.js
@@ -11,24 +11,19 @@ const groupsRouter = require('./routers/studyGroup');
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: '*',
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
+app.use(cors());
 app.use(userRouter);
 app.use(notificationRouter);
 app.use(groupsRouter);
 
-/*
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Headers", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Origin", "https://salmon-wave-0f89c260f.4.azurestaticapps.net/");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
     next();
-}); */
+});
 
 const port = process.env.PORT || 3000;
 
