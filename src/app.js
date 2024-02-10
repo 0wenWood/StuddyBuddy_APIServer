96684087f1ff441ea18718f49449b64e9c6e4f59
0wenWood <owenwood3435@outlook.com>
2024-02-10 11:13:51 -0500

    CORS Issue 3

--- src/app.js
+++ src/app.js
@@ -11,18 +11,24 @@ const groupsRouter = require('./routers/studyGroup');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 app.use(userRouter);
 app.use(notificationRouter);
 app.use(groupsRouter);
 
+/*
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Headers", "GET, POST, PATCH, DELETE");
     next();
-});
+}); */
 
 const port = process.env.PORT || 3000;
 
