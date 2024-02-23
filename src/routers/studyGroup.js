e8e5eac9eaa0f21047e8fb486a36a33814530837
0wenWood <owenwood3435@outlook.com>
2024-02-23 09:41:35 -0500

    Fixed Error Badrequest Bug

--- src/routers/studyGroup.js
+++ src/routers/studyGroup.js
@@ -9,18 +9,14 @@ router.post('/studygroup', auth, async (req, res) => {
     delete req.body.particpants
     delete req.body.owner
     const studyGroup = new StudyGroup({ ...req.body, owner: req.user._id });
-
+    console.log(studyGroup);
     try {
         await studyGroup.save();
         res.status(HTTPStatusCode.CREATED).send({ group: studyGroup, okay: true});
     } catch(e) {
+        console.log(e);
         res.status(HTTPStatusCode.BADREQUEST).send(e);
     }
 });
-
-router.get('/studygroup/all', async (req, res) => {
-    const studyGroups = await StudyGroup.find();
-    res.send(HTTPStatusCode.OKAY).send(studyGroups);
-})
 
 module.exports = router;
